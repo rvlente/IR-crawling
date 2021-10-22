10c3bfb3f96dd27f28ea6b0de3c784e605bad716
Nils Golembiewski <nilsgolembiewski@gmail.com>
2021-10-22 08:54:13 +0200

    better prio que

--- rust_crawl/src/main.rs
+++ rust_crawl/src/main.rs
@@ -1,15 +1,27 @@
+#![feature(map_first_last)]
+
 use anyhow::Result;
 use crossbeam::{channel, thread};
 use dashmap::{DashMap, DashSet};
 use parking_lot::Mutex;
-use std::{convert::TryFrom, path::{Path, PathBuf}, sync::{atomic::AtomicBool, Arc}, time::Duration};
+use std::{
+    collections::{BTreeMap, BTreeSet, HashSet},
+    convert::TryFrom,
+    iter::FromIterator,
+    net::IpAddr,
+    path::{Path, PathBuf},
+    sync::{atomic::AtomicBool, Arc},
+    time::{Duration, Instant},
+};
 // use parking_lot::Mutex;
+use rand::prelude::*;
 use rayon::prelude::*;
 use regex::Regex;
 use select::{document::Document, predicate::Name};
-use serde::{Deserialize, Serialize, };
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
 use static_init::dynamic;
 use std::collections::BinaryHeap;
+use std::fmt::Debug;
 use structopt::StructOpt;
 use tokio::runtime::Builder;
 
@@ -20,17 +32,85 @@ static WEB_RE: Regex = Regex::new(r#"https?://([^/]*)/?.*"#).unwrap();
 static DUTCH_URL: Regex = Regex::new(r#".*\Wnl\W.*"#).unwrap();
 
 #[dynamic]
-static SAVE_FILE: PathBuf = Path::new("cache/state.json").to_owned();
-
-#[dynamic]
 static RESULTS_FILE: PathBuf = Path::new("cache/results.txt").to_owned();
+
+/// Priority queue optimized for many items with the same priority
+#[derive(Debug, Default, Clone, Serialize, Deserialize)]
+struct UrlPrioQue {
+    que: BTreeMap<usize, Vec<String>>,
+}
+
+impl UrlPrioQue {
+    fn new() -> Self {
+        Self::default()
+    }
+
+    fn insert(&mut self, k: usize, v: String) {
+        self.que
+            .entry(k)
+            .or_insert_with(|| Default::default())
+            .push(v);
+    }
+
+    fn pop(&mut self) -> Option<String> {
+        let first_entry = self.que.first_entry();
+        if let Some(mut e) = first_entry {
+            if let Some(url) = e.get_mut().pop() {
+                if e.get().is_empty() {
+                    self.que.pop_first();
+                }
+                return Some(url);
+            }
+        }
+        None
+    }
+
+    fn extend(&mut self, items: impl IntoIterator<Item = (usize, String)>) {
+        for (c, u) in items {
+            self.insert(c, u);
+        }
+    }
+
+    fn shuffle_inner(&mut self) {
+        self.que
+            .iter_mut()
+            .for_each(|(_, sites)| sites.shuffle(&mut rand::thread_rng()));
+    }
+}
+
+impl FromIterator<(usize, String)> for UrlPrioQue {
+    fn from_iter<T: IntoIterator<Item = (usize, String)>>(iter: T) -> Self {
+        let mut s = Self::default();
+        for (c, u) in iter {
+            s.insert(c, u);
+        }
+        s
+    }
+}
+
+#[test]
+fn test_url_prioque() {
+    let mut q = UrlPrioQue::new();
+
+    q.insert(5, "5".into());
+    q.insert(1, "1".into());
+    q.insert(1, "1".into());
+    q.insert(1, "1".into());
+    q.insert(7, "7".into());
+
+    assert_eq!(q.pop(), Some("1".into()));
+    assert_eq!(q.pop(), Some("1".into()));
+    assert_eq!(q.pop(), Some("1".into()));
+    assert_eq!(q.pop(), Some("5".into()));
+    assert_eq!(q.pop(), Some("7".into()));
+}
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 struct DocData {
     text: String,
     url: String,
-    langs: Vec<String>,
-    links: Vec<String>,
+    langs: HashSet<String>,
+    links: HashSet<String>,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
@@ -51,15 +131,13 @@ impl Ord for HeapUrl {
     }
 }
 
-
-
 enum WorkerCmd {
     Stop,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Crawler {
-    que: Mutex<BinaryHeap<HeapUrl>>,
+    que: Mutex<UrlPrioQue>,
     history: DashSet<String>,
     being_processed: DashSet<String>,
     dutch_webpages: Mutex<Vec<String>>,
@@ -73,7 +151,7 @@ impl Crawler {
         save_file: impl Into<Option<PathBuf>>,
     ) -> Self {
         Self {
-            que: Mutex::new(start_urls.into_iter().map(|u| HeapUrl{ url: u, host_count: 0 }).collect()),
+            que: Mutex::new(start_urls.into_iter().map(|u| (0, u)).collect()),
             history: DashSet::new(),
             being_processed: DashSet::new(),
             dutch_webpages: Default::default(),
@@ -112,16 +190,13 @@ impl Crawler {
 
     fn mk_heap_url(&self, url: String) -> HeapUrl {
         let host_count = self.get_host_count(&url);
-        HeapUrl {
-            url,
-            host_count,
-        }
+        HeapUrl { url, host_count }
     }
 
-    async fn get_doc_data(url: &str) -> Result<DocData> {
-        
+    async fn make_request(url: &str) -> Result<String> {
         let client = reqwest::blocking::Client::builder()
-            .timeout(Duration::from_secs(30)).build()?;
+            .timeout(Duration::from_secs(30))
+            .build()?;
 
         // client.ge
 
@@ -131,7 +206,11 @@ impl Crawler {
 
         // let resp = reqwest::get(url).await?;
         // let txt = resp.text().await?;
+        Ok(txt)
+    }
 
+    async fn get_doc_data(url: &str) -> Result<DocData> {
+        let txt = Self::make_request(url).await?;
         let doc = Document::from(txt.as_str());
         // let doc = Document::from_read(resp)?;
 
@@ -146,21 +225,19 @@ impl Crawler {
             .find(Name("a"))
             .filter_map(|n| n.attr("href"))
             .map(ToOwned::to_owned)
-            .filter_map(|link| {
-                match url::Url::parse(&link) {
-                    Ok(parsed) => {
-                        if parsed.scheme().starts_with("http") {
-                            Some(link)
-                        } else {
-                            None
-                        }
-                    },
-                    Err(url::ParseError::RelativeUrlWithoutBase) => {
-                        let url_parsed = url::Url::parse(url).ok()?;
-                        Some(url_parsed.join(&link).ok()?.to_string())
+            .filter_map(|link| match url::Url::parse(&link) {
+                Ok(parsed) => {
+                    if parsed.scheme().starts_with("http") {
+                        Some(link)
+                    } else {
+                        None
                     }
-                    Err(_) => None
                 }
+                Err(url::ParseError::RelativeUrlWithoutBase) => {
+                    let url_parsed = url::Url::parse(url).ok()?;
+                    Some(url_parsed.join(&link).ok()?.to_string())
+                }
+                Err(_) => None,
             })
             .filter(|u| url::Url::parse(u).is_ok())
             .collect();
@@ -173,20 +250,21 @@ impl Crawler {
         })
     }
 
+    fn increment_host_count(&self, url: &str) {
+        if let Ok(url_parsed) = url::Url::parse(url) {
+            if let Some(host) = url_parsed.host_str() {
+                *self.domain_counts.entry(host.to_owned()).or_insert(0) += 1;
+            }
+        }
+    }
+
     async fn process_url(&self, url: &str) {
-        
         if let Ok(data) = Self::get_doc_data(&url).await {
             if !self.is_dutch_doc(&data) {
                 return;
             }
 
             println!("{}", data.url);
-
-            if let Ok(url_parsed) = url::Url::parse(url) {
-                if let Some(host) = url_parsed.host_str() {
-                    *self.domain_counts.entry(host.to_owned()).or_insert(0) += 1;
-                }
-            }
 
             self.dutch_webpages.lock().push(data.url);
 
@@ -202,15 +280,21 @@ impl Crawler {
                 self.history.insert(url);
             }
 
+            let extension: Vec<_> = filtered_urls
+                .into_iter()
+                .map(|u| {
+                    let hc = self.get_host_count(&u);
+                    self.increment_host_count(&u);
+                    (hc, u)
+                })
+                .collect();
 
-            self.que.lock().extend(filtered_urls.into_iter().map(|u| self.mk_heap_url(u)));
+            self.que.lock().extend(extension);
         }
     }
 
     async fn worker(&self, cmd_recv: channel::Receiver<WorkerCmd>) {
-        
         loop {
-            
             if let Ok(WorkerCmd::Stop) = cmd_recv.try_recv() {
                 break;
             }
@@ -218,9 +302,9 @@ impl Crawler {
                 let r = self.que.lock().pop();
                 r
             } {
-                self.being_processed.insert(hurl.url.clone());
-                self.process_url(&hurl.url).await;
-                self.being_processed.remove(&hurl.url);
+                self.being_processed.insert(hurl.clone());
+                self.process_url(&hurl).await;
+                self.being_processed.remove(&hurl);
             } else {
                 tokio::time::sleep(Duration::from_millis(500)).await;
             }
@@ -251,7 +335,15 @@ impl Crawler {
         Ok(())
     }
 
+    async fn send_back_public_ip(sender: channel::Sender<Option<IpAddr>>) {
+        let _ = sender.send(public_ip::addr().await);
+    }
+
     async fn run(&self, n_workers: usize) {
+        let start_ip = public_ip::addr()
+            .await
+            .expect("Couldn't get IP address: unsafe");
+
         async_scoped::TokioScope::scope_and_block(|scope| {
             let (sender, receiver) = channel::unbounded();
 
@@ -260,27 +352,42 @@ impl Crawler {
                 scope.spawn(self.worker(recv_clone));
             }
 
-            loop {
-                std::thread::sleep(Duration::from_secs(30));
+            let mut lc: usize = 0;
 
+            let save = || {
                 if let Err(_) = self.save_if_save_file() {
                     eprintln!("FAILED SAVING STATE");
                 }
+            };
 
-                // self.que.lock().sort_by_key(|url| {
-                //     if let Ok(url_parsed) = url::Url::parse(url) {
-                //         if let Some(host) = url_parsed.host_str() {
-                //             let count = self.domain_counts.get(host).map(|n| *n);
-                //             return count.unwrap_or(usize::max_value());
-                //         }
-                //     }
-                //     return usize::max_value();
-                // });
+            loop {
+                lc += 1;
+                std::thread::sleep(Duration::from_secs(30));
 
-                if self.being_processed.is_empty() {
+                if lc % 5 == 0 {
+                    save();
+                    self.que.lock().shuffle_inner()
+                }
+
+                if lc % 5 == 0 {}
+
+                let (ip_sndr, ip_rcvr) = channel::unbounded();
+                scope.spawn(Self::send_back_public_ip(ip_sndr));
+                let cur_ip = ip_rcvr.recv_timeout(Duration::from_secs(5));
+
+                let same_ip = if let Ok(Some(ip)) = cur_ip {
+                    ip == start_ip
+                } else {
+                    false
+                };
+
+                if self.being_processed.is_empty() || !same_ip {
+                    eprintln!("STOPPING CRAWLER");
                     for _ in 0..n_workers {
                         sender.send(WorkerCmd::Stop).unwrap();
                     }
+                    save();
+                    break;
                 }
 
                 eprintln!("Dutch sites found: {}", self.dutch_webpages.lock().len());
@@ -289,32 +396,41 @@ impl Crawler {
     }
 }
 
-
 #[derive(Debug, StructOpt)]
 struct Opt {
-    #[structopt(short, long, help="File to load and store state from/to")]
+    #[structopt(short, long, help = "File to load and store state from/to")]
     save_file: Option<PathBuf>,
-    #[structopt(long, help="amount of simultaneous workers", default_value="128")]
+    #[structopt(long, help = "amount of simultaneous workers", default_value = "128")]
     num_workers: usize,
 }
-
-
 
 fn main() {
     let opt = Opt::from_args();
 
-
-    let crawler = opt.save_file.clone()
+    let crawler = opt
+        .save_file
+        .clone()
         .and_then(|p| std::fs::read_to_string(p).ok())
         .and_then(|state| {
-            serde_json::de::from_str(&state).ok()
+            let mut result = serde_json::de::from_str(&state).ok();
+            result = result.map(|mut c: Crawler| {
+                c.save_file = opt.save_file.clone().into();
+                c
+            });
+            result
         })
-        .unwrap_or_else(|| Crawler::new(["https://www.wikipedia.nl/".to_string()], opt.save_file.clone()));
-    
+        .unwrap_or_else(|| {
+            Crawler::new(
+                ["https://www.wikipedia.nl/".to_string()],
+                opt.save_file.clone(),
+            )
+        });
+
     let runtime = Builder::new_multi_thread()
         .max_blocking_threads(opt.num_workers)
         .enable_all()
-        .build().unwrap();
+        .build()
+        .unwrap();
 
     // let crawler = Crawler::new(["https://www.wikipedia.nl/".to_string()], opt.save_file);
     runtime.block_on(crawler.run(opt.num_workers))
@@ -324,6 +440,16 @@ fn main() {
 fn test_url() {
     let a = url::Url::parse("https://help.com/Python.html").unwrap();
     let a = a.join("/oi").unwrap();
-    eprintln!("{:#?}",a);
+    eprintln!("{:#?}", a);
     eprintln!("{}", a);
+}
+
+#[tokio::test]
+async fn test_get_ip_addr() {
+    // Attempt to get an IP address and print it.
+    if let Some(ip) = public_ip::addr().await {
+        println!("public ip address: {:?}", ip);
+    } else {
+        println!("couldn't get an IP address");
+    }
 }
--- rust_crawl/tmptmp.py
+++ rust_crawl/tmptmp.py
@@ -1,6 +1,23 @@
-import json
+import orjson
+from collections import defaultdict
+from tqdm import tqdm
 
 with open("cache/state.json", "r") as f:
-    state = json.load(f)
+    state = orjson.loads(f.read())
 
-print(*state["que"], sep="\n")
+
+new_que_inner = defaultdict(list)
+
+for heapurl in tqdm(state["que"]):
+    new_que_inner[str(heapurl["host_count"])].append(heapurl["url"])
+
+# new_que_inner = dict(**new_que_inner)
+
+new_que = {
+    "que": new_que_inner
+}
+
+state["que"] = new_que
+
+with open("cache/state3.json", "wb") as f:
+    f.write(orjson.dumps(state))
